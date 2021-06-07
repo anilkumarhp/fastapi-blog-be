132e217a425bb03de75b9f1a34d6cadb97b7628d
Anil Kumar H P <anilkumarhp@users.noreply.github.com>
2021-06-07 21:47:23 +0530

    database connection added

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import uvicorn
 from fastapi import FastAPI
+from sqlalchemy.orm import Session
+
+from database.db import engine
+from models import model
+from schemas import schema
 
 app = FastAPI()
+
+model.Base.metadata.create_all(engine)
 
 
 @app.get("/")
 def read_root():
     return "It is working!!"
 
+
+@app.post("/")
+def create(request: schema.Blog, db: Session):
+    return db
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=5000, reload=True)
